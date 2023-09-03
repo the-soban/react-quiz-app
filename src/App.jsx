24b60134d9a1cee136bcfcf486ea9e5fc913807f
the-soban <sobanshafeq123@gmail.com>
2023-09-04 02:11:45 +0500

    added basic styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,29 @@
-import { useState } from "react";
-import "./App.css";
+import React, { useState } from "react";
+import "./App.scss";
 
 function App() {
     const [count, setCount] = useState(0);
 
     return (
         <div className="app">
-            <h1 className="header">Quiz APP</h1>
+            <h1 className="header">Quiz App</h1>
             <h2 className="score">Your Score: 3</h2>
             <div className="question-card">
                 <h2>Question 1 out of 5</h2>
-                <h3>What id the capital of the U.S?</h3>
+                <h3 className="question-text">
+                    What id the capital of the U.S?
+                </h3>
                 <ul>
                     <li>Boston</li>
                     <li>New York City</li>
                     <li>Denver</li>
                     <li>Washington DC</li>
                 </ul>
+            </div>
+            <div className="results">
+                <h2>Final Results</h2>
+                <h3>3 out of 5 correct! - 60%</h3>
+                <button>Restart</button>
             </div>
         </div>
     );
