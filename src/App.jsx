07f22c607316d3770f465b546986293eabe0492a
the-soban <sobanshafeq123@gmail.com>
2023-09-04 01:57:35 +0500

    completed dummy data setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0);
 
-  return (
-    <div className='app'>
-    </div>
-  )
+    return (
+        <div className="app">
+            <h1 className="header">Quiz APP</h1>
+            <h2 className="score">Your Score: 3</h2>
+            <div className="question-card">
+                <h2>Question 1 out of 5</h2>
+                <h3>What id the capital of the U.S?</h3>
+                <ul>
+                    <li>Boston</li>
+                    <li>New York City</li>
+                    <li>Denver</li>
+                    <li>Washington DC</li>
+                </ul>
+            </div>
+        </div>
+    );
 }
 
-export default App
+export default App;
